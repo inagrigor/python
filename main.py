c87095f47434ec65d5cc614cc9d8e2c0c9cda6c5
Grigor_Ina <grigorina72@gmail.com>
2024-03-26 06:09:28 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,196 @@
+# n = 1.89
+
+# n = 'frd"cadse"caxz'
+# n = 'frd\'cax'
+#n = "vffrb"
+# print(type(n))
+# print(n)
+# print(5)
+# print(5)
+# print(5,4)
+"""
+print(23)
+print(87)
+print(13)
+"""
+# print(23)
+# print(87)
+# print(13)
+
+# a = 5
+# b = 2.13
+# c = 'hello'
+
+# print(a, b, c)
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a, b, c))
+
+# introducerea tipurilor de date (functii)
+# print('introduceti primul rind')
+# print('Intoducieti primul numar: ')
+# a = input()
+# print(a)
+# b = input('Introduceti al doilea numar:')
+
+# Adunarea rindurilor
+# print('Intoducieti primul numar: ')
+# a = input()
+# b = input('Introduceti al doilea numar:')
+
+# print(a, ' + ', b, ' = ', a + b)
+
+# c = 5.65
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+# c = str(c)
+# print(c +'cdase')
+# print(type(c))
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# adunarea
+# print('Intoducieti primul numar: ')
+# a = int (input())
+# b = int (input('Introduceti al doilea numar:'))
+
+# print(a, ' + ', b, ' = ', a + b)
+
+# rotungirea
+# a = 4.54633
+# b = 2.58743
+# print(round(a*b, 2))
+# round(1.35421, 3) - intregul si numar de cifre dupa virgula
+
+# iter = 2
+# iter += 1
+# iter -= 1
+# iter /= 1
+# iter //= 1
+# iter %= 1
+# iter *= 1
+# iter **= 1
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 == 2
+# print(a)
+# a = 1 != 2 and 5 != 2
+# print(a)
+# a = 'cbvd'
+# b = 'cbvd'
+# print(a == b)
+# a = 1 < 3 < 5 < 10
+# print (a)
+
+# constructie if, if-else
+# if condition1:
+#     operator
+# elif condition2:
+#     operator2
+# elif condition3: 
+#     operator3
+# else:
+#     operator4
+
+# username = input('Introduceti numele:')
+# if username == 'Masha':
+#     print('Salut, Masha')
+# elif username == 'Marina':
+#     print('Salut, Marina')
+# elif username == 'Ilnar':
+#     print('Salut, Ilnar')
+# else: 
+#     print('Salut, ', username)
+
+# n = 432
+# sum = 0
+# while n > 0:
+#     x = n % 10
+#     n = n // 10
+#     sum = sum + x
+# print(sum)
+
+# i = 0
+# while i < 5:
+#     if i == 3:
+#         break
+#     i += 1
+# else:
+#     print('cred ca')
+#     print('e suficient')
+# print(i)
+
+# i = 0
+# while i < 5: 
+   
+#     i += 1
+# else:
+#     print('cred ca')
+#     print('e suficient')
+# print(i)
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n // 2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+# for i in 1, -2, 3, 14, 15:
+#     print(i)
+# r = range(100, 0, -20)
+# for i in r:
+#     print(i)
+
+# a = 'asdfc'
+# for i in a:
+#     print(i)
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"
+#     print(line)
+#text = 'Mai maninca cruasan acest moale'
+# print (len(text))
+# print('mai' in text)
+# print('Mai' in text)
+# print(text.lower())
+# print(text.upper())
+# print(text.replace('Mai', 'MAI'))
+
+# srezi
+
+# print(text[0])
+# print(text[1])
+#print(text[len(text)-1]) # =  print (text[-1])
+# print (text[-1])
+# print(text[-5])
+# print(text[:])
+# print(text[:2]) # de 0 pina la [2]
+#print (text[len(text)-2:])  # de la [-2] pna la urma, daca nu este indicata cifra dupa :
+#print(text[20:])
+# print(text[2:9])
+# print(text[6:-18])
+# print (text[0: len(text):6]) = print(text[::6]) # de la 0 pe lungimea str cu pasul 6 (adica fiecarea a 6-le element)
+# print(text[::6])
+# text = text[2:9] + text[-8] + text[:10] + text[2:9]
+# print(text)
+
+
+
