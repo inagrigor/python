26c9345a89cfb6c2b24bf8b56e04840e1657bacb
Grigor_Ina <grigorina72@gmail.com>
2024-03-28 12:08:28 +0200

    add_2 test_2

--- test2.py
+++ test2.py
@@ -76,6 +76,186 @@
 #     if list[i] > list[i - 1]:
 #         count += 1
 # print (count)
-       
+
+# tema de acasa
+# Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N]. 
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+
+# N = int(input())
+# list = []
+# for i in range(1, N + 1):
+#     list.append(i)
+# print (list)
+# X = int(input())
+# count = 0
+
+# for i in list:
+#     if i == X:
+#         count += 1 
+# print (count)
     
+#     TEMA DE ACASA II
+#    Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X. 
+#    Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+#    В последующих  строках записаны N целых чисел Ai . 
+#    Последняя строка содержит число X           
+    
+# N = int(input())
+# list_1 = []
+
+# for i in range(1, N+1):
+#     list_1.append(i)
+# print(list_1)
+
+# x = int(input())
+# с = list_1[0]
+
+# min_diff = N + 1
+
+# for number in list_1:
+#     diff = number - x
+#     if 0 <= diff < min_diff:
+#         min_diff = diff
+#         c = number
+#         print (c)  
+   
+# list_1 = [1, 12, 6, 7, 8, 15]   
+# k = 11  
+# number = 0 # инициализация переменной которая будет хранить ближайший елемент к х
+# for i in range(len(list_1)): 
+#     if k - list_1[i] < k - number and k - list_1[i] >= 0:
+#         number = i
+# print(list_1[number]) 
+
+# number = 0 if list_1[0] <= k else len(list_1) - 1
+
+# for i in range(len(list_1)):
+#     if list_1[i] == k:
+#         number = i
+#         break
+#     elif list_1[i] < k and k - list_1[i] < k - list_1[number]:
+#         number = i
+#     elif list_1[i] > k and list_1[i] - k < list_1[number] - k:
+#         number = i
+
+# print(list_1[number])
+# list_1 = [1, 12, 6, 7, 8, 15]
+# k = 11
+# number = 0  # Инициализация переменной, которая будет хранить индекс ближайшего элемента к k
+
+# for i in range(1, len(list_1)):
+#     if abs(k - list_1[i]) < abs(k - list_1[number]):
+#         number = i
+
+# print(list_1[number])
+
+
+
+# points_ru = {1:"АВЕИНОРСТ", 2:"ДКЛМПУ", 3:"БГЁЬЯ", 4:"ЙЫ", 5:"ЖЗХЦЧ", 8:"ШЭЮ", 10:"ФЩЪ"}
+# points_en = {1:"AEIOULNSTR", 2:"DG", 3:"BCMP", 4:"FHVWY", 5:"K", 8:"JX", 10:"QZ"}
+
+# word = "ноутбук"
+
+# if word[0].lower() in points_ru:
+#         sum = 0
+#         for letter in word:
+#             for key, value in points_ru.items():
+#                 if letter.upper() in value:
+#                     sum += key
+
+#         print(sum)
+
+# elif word[0].lower() in points_en:
+#         sum = 0
+#         for letter in word:
+#             for key, value in points_en.items():
+#                 if letter.upper() in value:
+#                     sum += key
+#         print(sum)
+    
+# points_ru = {"АВЕИНОРСТ":1, "ДКЛМПУ":2, "БГЁЬЯ":3, "ЙЫ": 4, "ЖЗХЦЧ":5, "ШЭЮ":8, "ФЩЪ":10}
+# points_en = {"AEIOULNSTR":1, "DG":2, "BCMP":3, "FHVWY":4, "K":5, "JX":8, "QZ":10}
+
+# word = "ноутбук"
+# word_upper = word[0].upper()  # Приводим первую букву слова к нижнему регистру для сравнения
+
+# if word_upper in points_ru:
+#     total_score = 0
+#     for letter in word:
+#         for key, value in points_ru.items():
+#             if letter.upper() in key:
+#                 total_score += value
+#                 break
+#     print("Сумма очков для слова '{}' на русском языке: {}".format(word, total_score))
+
+# elif word_upper in points_en:
+#     total_score = 0
+#     for letter in word:
+#         for key, value in points_en.items():
+#             if letter.upper() in key:
+#                 total_score += value
+#                 break
+#     print("Сумма очков для слова '{}' на английском языке: {}".format(word, total_score))
+
+# else:
+#     print("Ошибка: Начальная буква '{}' не определена в словарях.".format(word_upper))
+    
+# points_ru = {"АВЕИНОРСТ":1, "ДКЛМПУ":2, "БГЁЬЯ":3, "ЙЫ": 4, "ЖЗХЦЧ":5, "ШЭЮ":8, "ФЩЪ":10}
+#  points_en = {"AEIOULNSTR":1, "DG":2, "BCMP":3, "FHVWY":4, "K":5, "JX":8, "QZ":10}
+
+# k = "ноутбук"
+#  k_lower = k[0].lower()  # Приводим первую букву слова к нижнему регистру для сравнения
+
+#  # Определяем, принадлежит ли начальная буква слова русскому или английскому алфавиту
+#  if any(k_lower in key.lower() for key in points_ru):
+#      total_score = 0
+#      for letter in k:
+#         for key, value in points_ru.items():
+#             if letter.upper() in key:
+#                 total_score += value
+#                 break
+#      print(total_score)
+
+#  elif any(k_lower in key.lower() for key in points_en):
+#     total_score = 0
+#     for letter in k:
+#         for key, value in points_en.items():
+#             if letter.upper() in key:
+#                 total_score += value
+#                 break
+#     print(total_score)
+
+#  else:
+#     print("Ошибка: Начальная буква '{}' не определена в словарях.".format(word_lower))
+
+# points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+# points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+# word = k.upper()  # переводим все буквы в верхний регистр
+# count = 0
+# for i in word:
+#     if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+#         for j in points_en:
+#             if i in points_en[j]:
+#                 count = count + j
+#     else:
+#         for j in points_ru:
+#             if i in points_ru[j]:
+#                 count = count + j
+# print(count)
+
+
+# word = k.upper()
+# count = 0
+# for i in word:
+#     if i in "QWERTYUIOPASDFGHJKLZXCVBNM":
+#         for j in points_en:
+#             if i in points_en[j]:
+#                 count = count + j
+#     else:
+#         for j in points_ru:
+#             if i in points_ru[j]:
+#                 count = count + j
+# print(count)
+            
     
