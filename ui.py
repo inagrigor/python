68372cea2dad6c05dc89c8d888ba629f5a7da972
Grigor_Ina <grigorina72@gmail.com>
2024-04-19 16:22:16 +0300

    add ui

--- ui.py
+++ ui.py
@@ -1,4 +1,16 @@
 
-
+from logger2 import input_data, print_data
 def interface():
-    print ("Buna ziua ? ati nimerit la botul informa'ional a lui GeekBrains ")
+    print ("Buna ziua, ati nimerit la botul informational a lui GeekBrains \n 1 - introducerea datelor \n 2 - extragerea de date")
+    command = int(input('introduceti numarul '))  # variabila prin care primim date de la consoli
+    while command != 1 and command != 2:
+        print ("numarul nu este introds corect ")
+        command = int(input('introduceti numarul '))
+
+    if command == 1:
+        input_data()
+    elif command == 2:
+        print_data()
+    
+
+        
